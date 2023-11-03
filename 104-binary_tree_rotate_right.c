f57a394c8669857395e4dcd42e700a15cac3cbbf
knwble <noble4peter1@gmail.com>
2023-11-03 22:45:14 +0100

    rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -7,18 +7,30 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *new_root, *tmp;
+
 	if (tree == NULL || tree->left == NULL)
-		return (tree);
+		return (NULL);
 
-	binary_tree_t *new_root = tree->left;
-
-	tree->left = new_root->right;
+	new_root = tree->left;
+	tmp = new_root->right;
 	new_root->right = tree;
-	new_root->parent = tree->parent;
-	tree->parent = new_root;
+	tree->left = tmp;
 
-	if (tree->left)
-		tree->left->parent = tree;
+	if (tmp != NULL)
+		tmp->parent = tree;
+
+	tmp = tree->parent;
+	tree->parent = new_root;
+	new_root->parent = tmp;
+
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = new_root;
+		else
+			tmp->right = new_root;
+	}
 
 	return (new_root);
 }
